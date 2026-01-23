f4d81a504049c707f6407d6f29da3f3c9d71ad3a
vetups <sercherufa@gmail.com>
2026-01-23 11:01:26 +0500

    Реализация функционала окна авторизации

--- ShoeAccounting/ShoeAccounting/Views/Windows/MainWindow.xaml.cs
+++ ShoeAccounting/ShoeAccounting/Views/Windows/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using ShoeAccounting.Models;
+using ShoeAccounting.Utils;
 using ShoeAccounting.Views.Windows;
 using System.Data.Common;
 using System.Text;
@@ -26,35 +27,57 @@ namespace ShoeAccounting
 
         private void enterButton_Click(object sender, RoutedEventArgs e)
         {
+            string login = loginTextBox.Text.Trim();
+            string password = passwordTextBox.Text.Trim();
+
+            string? errorMessage = ValidateLoginData(login, password);
+            if (errorMessage != null)
+                MessageBox.Show(errorMessage, "Ошибка валидации", MessageBoxButton.OK, MessageBoxImage.Error);
+            else
+            {
+                User? user = LoginUser(login, password);
+                if (user == null)
+                    MessageBox.Show("Неверный логин или пароль", "Ошибка авторизации", MessageBoxButton.OK, MessageBoxImage.Error);
+                else
+                {
+                    UserContext.CurrentUser = user;
+                    MessageBox.Show("Добро пожаловать!", "Успешный вход", MessageBoxButton.OK, MessageBoxImage.Information);
+                    NavigateToCatalog();
+                }
+            }
 
         }
 
-        private string? CheckLoginData()
+        private string? ValidateLoginData(string login, string password)
         {
-            string loginText = loginTextBox.Text.Trim();
-            string passwordText = passwordTextBox.Text.Trim();
-
-            if (string.IsNullOrEmpty(loginText))
+            if (string.IsNullOrEmpty(login))
                 return "Логин не может быть пустым";
 
-            else if (string.IsNullOrEmpty(passwordText))
+            else if (string.IsNullOrEmpty(password))
                 return "Пароль не может быть пустым";
-
-            using (ShoesDbContext context = new ShoesDbContext())
-            {
-                User? user = context.Users.FirstOrDefault(u => u.UserLogin == loginText && u.UserPassword == passwordText);
-
-                if (user == null)
-                    return "Неверный логин или пароль";
-            }
 
             return null;
         }
 
-        private void catalogButton_Click(object sender, RoutedEventArgs e)
+        private User? LoginUser(string login, string password)
+        {
+            using (ShoesDbContext context = new ShoesDbContext())
+            {
+                User? user = context.Users.FirstOrDefault(u => u.UserLogin == login && u.UserPassword == password);
+
+                return user == null ? null:user;
+            }
+        }
+
+        private void NavigateToCatalog()
         {
             new CatalogWindow().Show();
             Close();
+        }
+
+        private void catalogButton_Click(object sender, RoutedEventArgs e)
+        {
+            NavigateToCatalog();
         }
     }
 }
